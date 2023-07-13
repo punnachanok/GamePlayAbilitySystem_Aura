d267e64f70900ae43edc0fdc446dbde51d85bf8d
unknown <punnachanok@gmail.com>
2023-07-13 17:52:58 +0700

    End of Chapter 1

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -5,10 +5,77 @@
 
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
+#include "Interaction/EnemyInterface.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
 	bReplicates = true;
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	CursorTrace();
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	GetHitResultUnderCursor(ECC_Visibility, false, CursorHit);
+	if(!CursorHit.bBlockingHit) return;
+
+	LastActor = ThisActor;
+	ThisActor = Cast<IEnemyInterface>(CursorHit.GetActor());
+
+	/**
+	 * Line trace from cursor. There are several scenarios:
+	 * A. LastActor is null && This ThisActor is null
+	 *	   - Do nothing.
+	 * B. LastActor is null && ThisActor is valid
+	 *     - Highlight ThisActor
+	 * C. LastActor is valid && ThisActor is null
+	 *     - UnHighlight LastActor
+	 * D. Both are valid, but LastActor != This Actor
+	 *     - Unhighlight LastActor, and Highlight ThisActor
+	 * E. Both actors are valid, and are the same actor
+	 *     - Do nothing
+	 */
+
+if(LastActor == nullptr)
+{
+	if(ThisActor != nullptr)
+	{
+		//Case B
+		ThisActor->HighlightActor();
+		
+	}
+	else
+	{
+		// Case A - both are null, do nothing
+	}
+}
+	else // LastActor is valid
+	{
+		if(ThisActor == nullptr)
+		{
+			// Case C
+			LastActor->UnHighlightActor();
+		}
+		else // both actors are valid
+		{
+			if(LastActor != ThisActor)
+			{
+				// Case D
+				LastActor->UnHighlightActor();
+				ThisActor->HighlightActor();
+			}
+			else
+			{
+					// Case E - do nothing
+			}
+		}
+	}
 }
 
 void AAuraPlayerController::BeginPlay()
@@ -45,13 +112,14 @@ void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
 	const FRotator Rotation = GetControlRotation();
 	const FRotator YawRotation(0.f, Rotation.Yaw, 0.f);
 
-	const FVector FowardDirectinon = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-	const FVector RightDirectinon = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
 
 	if(APawn* ControlledPawn = GetPawn<APawn>())
 	{
-		ControlledPawn->AddMovementInput(FowardDirectinon, InputAxisVector.Y);
-		ControlledPawn->AddMovementInput(RightDirectinon, InputAxisVector.X);
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
 	}
 	
 }
+
